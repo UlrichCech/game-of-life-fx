f2dee82846df89366aad13407e30a5a5b29bcdc7
Ulrich Cech <ulrich.cech@dreamit.de>
2015-01-14 18:35:11 +0100

    GameRules: - added rules of the game

---
+++ src/main/java/de/sofiworx/gameoflife/GameRules.java
@@ -0,0 +1,32 @@
+package de.sofiworx.gameoflife;
+
+/**
+ * Represents the rules of the game.
+ *
+ * @author Ulrich Cech
+ */
+public class GameRules {
+
+    private static final GameRules instance = new GameRules();
+
+    private GameRules() {}
+
+
+    public static GameRules getInstance() {
+        return instance;
+    }
+
+
+    public boolean isCellAliveInNextGeneration(final boolean currentAlive, final int amountAliveNeighbors) {
+        if ((amountAliveNeighbors == 1) || (amountAliveNeighbors > 3)) {
+            return false;
+        }
+        if (amountAliveNeighbors == 3) {
+            return true;
+        }
+        if (amountAliveNeighbors == 2) {
+            return currentAlive;
+        }
+        return currentAlive;
+    }
+}
---
+++ src/test/java/de/sofiworx/gameoflife/GameRulesTest.java
@@ -0,0 +1,26 @@
+package de.sofiworx.gameoflife;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+
+public class GameRulesTest {
+
+    @Test
+    public void testGetInstance() {
+        GameRules rules = GameRules.getInstance();
+        assertSame(rules, GameRules.getInstance());
+    }
+
+    @Test
+    public void isAliveInNextGeneration() {
+        GameRules rules = GameRules.getInstance();
+        assertThat(rules.isCellAliveInNextGeneration(true, 1), Is.is(false));
+        assertThat(rules.isCellAliveInNextGeneration(true, 2), Is.is(true));
+        assertThat(rules.isCellAliveInNextGeneration(true, 3), Is.is(true));
+        assertThat(rules.isCellAliveInNextGeneration(false, 3), Is.is(true));
+        assertThat(rules.isCellAliveInNextGeneration(true, 4), Is.is(false));
+    }
+}
